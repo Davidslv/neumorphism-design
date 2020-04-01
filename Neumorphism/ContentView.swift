d42763493a5ed62abcab2a1fb376991160e9da55
David Silva <davidslv@gmail.com>
2020-04-01 22:55:39 +0100

    A nice NeuMorphism effect
    We are exploring NeuMorphism Design.
    
    NeuMorphism is a relatively new design trend and term.
    The idea is to make the UI elements look like they are part of the background, and so the technique uses
    shadows, blur and shapes to appear smooth.
    
    We are doing everything programatically in here.

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ContentView.swift
 //  Neumorphism
 //
@@ -7,10 +7,96 @@
 //
 
 import SwiftUI
+ 
+struct SimpleButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(30)
+            .contentShape(Circle())
+            .background(
+                Group {
+                    if configuration.isPressed {
+                        Circle()
+                            .fill(Color.offWhite)
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.gray, lineWidth: 4)
+                                    .blur(radius: 4)
+                                    .offset(x: 2, y: 2)
+                                .mask(
+                                    Circle()
+                                    .fill(LinearGradient(Color.black, Color.clear))
+                                )
+                            )
+                        .overlay(
+                            Circle()
+                                .stroke(Color.white, lineWidth: 8)
+                                .blur(radius: 4)
+                                .offset(x: -2, y: -2)
+                            .mask(
+                                Circle()
+                                .fill(LinearGradient(Color.clear, Color.black)))
+                        )
+                    }
+                    else {
+                        Circle()
+                            .fill(Color.offWhite)
+                            .shadow(
+                                color: Color.black.opacity(0.2),
+                                radius: 10,
+                                x: 10,
+                                y: 10
+                            )
+                            .shadow(
+                                color: Color.white.opacity(0.7),
+                                radius: 10,
+                                x: -5,
+                                y: -5
+                            )
+                    }
+                }
+        )
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack {            
+            ZStack {
+                // NeuMorphism Card
+                Color.offWhite
+                LinearGradient(Color.darkStart, Color.darkEnd)
+                
+                Button(action: {
+                    print("Btn Tapped")
+                }) {
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.gray)
+                }
+                .buttonStyle(SimpleButtonStyle())
+                .zIndex(1)
+
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.offWhite)
+                    .frame(width: 300, height: 300)
+                .zIndex(0)
+                .shadow(
+                    color: Color.black.opacity(0.2),
+                    radius: 10,
+                    x: 10,
+                    y: 10
+                )
+                .shadow(
+                    color: Color.white.opacity(0.66),
+                    radius: 10,
+                    x: -5,
+                    y: -5
+                )
+            }
+            .edgesIgnoringSafeArea(.all)
+        }
+        
     }
 }
 
---
+++ Neumorphism/Extensions/Color.swift
@@ -0,0 +1,29 @@
+//
+//  Color.swift
+//  Neumorphism
+//
+//  Created by David Silva on 01/04/2020.
+//  Copyright © 2020 David Silva. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static let offWhite = Color(
+        red: 225 / 255,
+        green: 225 / 255,
+        blue: 235 / 255
+    )
+    
+    static let darkStart = Color(
+        red: 50 / 255,
+        green: 60 / 255,
+        blue: 65 / 255
+    )
+    
+    static let darkEnd = Color(
+        red: 25 / 255,
+        green: 25 / 255,
+        blue: 30 / 255
+    )
+}
