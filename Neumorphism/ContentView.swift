039a60c51fb7af5a98f3d66970aaf5862a8d95e9
David Silva <davidslv@gmail.com>
2020-04-01 21:28:48 +0100

    Initial Commit

---
+++ Neumorphism/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Neumorphism
+//
+//  Created by David Silva on 01/04/2020.
+//  Copyright © 2020 David Silva. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
